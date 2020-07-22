9763946ce32cfcde657d43006621fe786b3ada0b
Charles-PC <ct354@kent.ac.uk>
2020-07-22 12:43:03 +0100

    UI Done

--- A_Star.py
+++ A_Star.py
@@ -18,17 +18,17 @@ ORANGE = (255, 165, 0)
 GREY = (128, 128, 128)
 TURQUOISE = (64, 244, 208)
 
+
 class Node:
     def __init__(self, row, col, width, total_rows):
         # Row and col position
         self.row = row
         self.col = col
-
-        self.x = col * width
+        self.x = row * width
         self.y = col * width
-
         self.colour = WHITE
         self.neighbours = []
+        self.width = width
         self.total_rows = total_rows
 
     # Get node position
@@ -53,9 +53,12 @@ class Node:
         return self.colour == TURQUOISE
 
     def reset(self):
-        self.colour == WHITE
+        self.colour = WHITE
 
     # ** Set node state **
+
+    def make_start(self):
+        self.colour = ORANGE
 
     def make_closed(self):
         self.colour = RED
@@ -89,6 +92,114 @@ def heuristic(p1, p2):
     x1, y1 = p1
     x2, y2 = p2
     return abs(x1 - x2) + abs(y1 - y2)
+
+
+# Data Structure creation
+def make_grid(rows, width):
+    grid = []
+    gap = width // rows  # Gives the width of each Node based on the width of the page
+    # Creat the list structure - List in List containing Nodes based on rows
+    for i in range(rows):
+        grid.append([])
+        for j in range(rows):
+            node = Node(i, j, gap, rows)  # new node(row, col, width, rows)
+            grid[i].append(node)
+
+    return grid
+
+
+# draw grid lines
+def draw_grid(win, rows, width):
+    gap = width // rows
+    for i in range(rows):
+        pygame.draw.line(win, GREY, (0, i * gap), (width, i * gap))  # Draw line each horizontal line
+        for j in range(rows):
+            pygame.draw.line(win, GREY, (j * gap, 0), (j * gap, width))  # Draw line each vertical line
+
+
+def draw(win, grid, rows, width):
+    win.fill(WHITE)  # Fill screen on new frame - normal for pygame
+
+    # Loop through every node in list(row) in list(grid)
+    for row in grid:
+        for node in row:
+            node.draw(win)  # Draw each node on the grid
+
+    draw_grid(win, rows, width)  # Draw the grid on the page
+    pygame.display.update()
+
+
+# Node location for click
+def get_clicked_pos(pos, rows, width):
+    gap = width // rows
+    y, x = pos
+
+    # Take pos of x , y and div by each of the cubes
+    row = y // gap
+    col = x // gap
+
+    return row, col
+
+
+# Main loop
+def main(win, width):
+    ROWS = 50  # Number of rows in the grip
+    grid = make_grid(ROWS, width)  # Make the grid
+
+    # Define start and end position
+    start = None
+    end = None
+
+    run = True
+    started = False
+
+    while run:
+        draw(win, grid, ROWS, width)
+        for event in pygame.event.get():  # Loop through events
+            if event.type == pygame.QUIT:  # If the close button is pressed, break loop
+                run = False
+
+            # If algorithm running, don't allow changes to be made
+            if started:
+                continue
+
+            if pygame.mouse.get_pressed()[0]:  # If the left mouse button is pressed
+                print("Left mouse clicked")
+                pos = pygame.mouse.get_pos()
+                row, col = get_clicked_pos(pos, ROWS, width)  # position in the game screen that was clicked
+                node = grid[row][col]
+
+                if not start and node != end: # If the start is not defined - set the clicked nod as start
+                    start = node
+                    start.make_start()
+
+                elif not end and node != start:  # If end is not defined set node as end
+                    end = node
+                    end.make_end()
+
+                elif node != end and node != start:  # If the start and end defined, set clicked node as barrier
+                    node.make_barrier()
+
+            elif pygame.mouse.get_pressed()[2]: # If the right mouse button is pressed
+                pos = pygame.mouse.get_pos()
+                row, col = get_clicked_pos(pos, ROWS, width)  # position in the game screen that was clicked
+                node = grid[row][col]
+                node.reset()
+                if node == start:
+                    start = None
+                elif node == end:
+                    end = None
+
+    pygame.quit()
+
+
+main(WIN, WIDTH)
+
+
+
+
+
+
 
 
 
